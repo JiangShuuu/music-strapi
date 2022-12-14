4e6bf452b90b000de2f60a01a20aa43d6a669221
JiangShuuu <globelex65@gmail.com>
2022-12-14 21:16:20 +0800

    first

---
+++ src/api/epsiode/controllers/epsiode.ts
@@ -0,0 +1,7 @@
+/**
+ * epsiode controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::epsiode.epsiode');
---
+++ src/api/epsiode/routes/epsiode.ts
@@ -0,0 +1,7 @@
+/**
+ * epsiode router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::epsiode.epsiode');
